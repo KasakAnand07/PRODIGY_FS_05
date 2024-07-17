72794ed4799f729eb59d859bab89fdc1312606d7
KasakAnand07 <kasakanand07@gmail.com>
2024-07-18 01:05:35 +0530

    final commit

--- client/src/utils/index.js
+++ client/src/utils/index.js
@@ -2,7 +2,8 @@
 import axios from "axios";
 import { SetPosts } from "../redux/postSlice";
 
-const API_URL = "http://localhost:8800";
+const API_URL = "https://klique-socialmedia.onrender.com";
+   //"http://localhost:8800";
 
 export const API = axios.create({
     baseURL: API_URL,
